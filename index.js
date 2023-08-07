21cf7a8841fc961c97ce8f9af26a1dfe7a8d205e
Vanessa Jesik <vajesik@gmail.com>
2023-08-07 16:44:06 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(array, name) {
+  const matchingDrivers = array.filter(
+    (driver) => driver.toUpperCase() === name.toUpperCase()
+  );
+  return matchingDrivers;
+}
+
+function fuzzyMatch(array, letters) {
+  const matchingLetters = array.filter((name) => {
+    return name.indexOf(letters) === 0;
+  });
+  return matchingLetters;
+}
+
+function matchName(array, name) {
+  const matchingDriverName = array.filter(
+    (nameAndHome) => nameAndHome.name === name
+  );
+  return matchingDriverName;
+}
